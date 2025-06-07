728d26f6ae96d6829b85896e84814f497ee460bb
LarissaMDC <larissamdc19@gmail.com>
2025-06-07 00:16:13 -0300

    Update Model.java

--- NovaVenda/Model.java
+++ NovaVenda/Model.java
@@ -1,1 +1,20 @@
+package VendasModel;
+
+public class ItemVenda {
+    private Produto produto;
+    private int quantidade;
+
+    public ItemVenda(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() { return produto; }
+    public int getQuantidade() { return quantidade; }
+
+    public double getSubtotal() {
+        return produto.getPreco() * quantidade;
+    }
+}
+}
 
